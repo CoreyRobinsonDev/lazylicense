4c342241bfbee6400fa80ec286236fe4fa723869
CoreyRobinsonDev <coreyrob52@gmail.com>
2024-12-24 09:50:32 -0500

    feat: add license menu

--- main.go
+++ main.go
@@ -2,11 +2,44 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
+const domain = "https://choosealicense.com"
+
 func main() {
+	licenses := GetLicenses()
+	names := make([]string, 0, len(licenses))
+
+	for _, license := range licenses {
+		name := license.Name
+		if license.AbbrName != "" {
+			name += " (" + license.AbbrName + ")"
+		}
+		names = append(names, name)
+	} 
+
+	InitInput()
+	moveNum := 0
+	selection := HighlightSelection(moveNum, names)
+	fmt.Println(strings.Join(selection, "\n"))
+
+	for {
+		MoveCursor("up", len(licenses))
+		MoveCursor("left", 99)
+		moveNum += DetectMove()
+		if moveNum < 0 {
+			moveNum = len(licenses) - 1
+		}
+		idx := moveNum % len(licenses)
+		selection := HighlightSelection(idx, names)
+		fmt.Println(strings.Join(selection, "\n"))
+	}
 }
+
 
 type License struct {
 	Name string
@@ -18,62 +51,48 @@ type License struct {
 	Limitations []string
 }
 
-// Returns the final box and width
-func box(text string) (string, int)  {
-	tl := "╭"
-	tr := "╮"
-	bl := "╰"
-	br := "╯"
-	h := "─"
-	v := "│"
-	pad := " "
-	h_line := h
-	text_seg := strings.Split(text, "\n")
-	max_len := len(text_seg[0])
-	paddings := make([]string, 0, 1)
-	var out string
 
-	for _, seg := range text_seg {
-		if max_len < len(seg) {
-			max_len = len(seg)
-		}
-	}
-
-	for range max_len {
-		h_line += h
-	}
-
-	for _, seg := range text_seg {
-		r_pad := pad
-		var diff int
-		if d := max_len - len(seg); d < 0 {
-			diff = 0
-		} else { diff = d }
-
-		for range diff {
-			r_pad += pad
-		}
-
-		paddings = append(paddings, r_pad)
-	}
-
-	h_line += h
-
-	out = fmt.Sprintf("%s%s%s\n",
-		tl, h_line, tr,
-	)
-
-	for i, seg := range text_seg {
-		out += fmt.Sprintf("%s%s%s%s%s\n",
-			v, pad, seg, paddings[i], v,
-		)
-	}
-
-	out += fmt.Sprintf("%s%s%s\n",
-		bl, h_line, br,
-	)
-
-	return out, max_len + 4
+func PrintLicenseDetails(license License) {
+	 detailsString := "Permissions\n\u2b24 " + strings.Join(license.Permissions, " \n\u2b24 ") + "\n\nConditions\n\u2b24 " + strings.Join(license.Conditions, " \n\u2b24 ") + "\n\nLimitations\n\u2b24 " + strings.Join(license.Limitations, " \n\u2b24 ")
+	fmt.Println(Box(detailsString))
 }
 
+
+func GetLicenses() []License {
+	res := Unwrap(http.Get(domain + "/licenses"))
+	defer res.Body.Close()
+	doc := Unwrap(goquery.NewDocumentFromReader(res.Body))
+	licenses := make([]License, 0)
+
+	doc.Find(".license-overview-name").Each(func(i int, s *goquery.Selection) {
+		route, exists := s.Find("a").Attr("href")
+		if !exists { handleErr("missing link on " + domain) }
+		page := Unwrap(http.Get(domain + route))
+		defer page.Body.Close()
+		pageDoc := Unwrap(goquery.NewDocumentFromReader(page.Body))
+		license := new(License)
+
+		license.Name = strings.Trim(pageDoc.Find("h1").Text(), " \t\n")
+		license.AbbrName = strings.Trim(pageDoc.Find(".license-nickname").Text(), " \t\n")
+		var childOffset int
+		if license.AbbrName == "" {
+			childOffset = 1
+		} else { childOffset = 2 }
+		license.Description = strings.Trim(pageDoc.Find(fmt.Sprintf("div.license-body > p:nth-child(%d)", childOffset)).Text(), " \t\n")
+		pageDoc.Find("ul.license-permissions > li").Each(func(i int, s *goquery.Selection) {
+			license.Permissions = append(license.Permissions, strings.Trim(s.Text(), " \t\n"))
+		})
+		pageDoc.Find("ul.license-conditions > li").Each(func(i int, s *goquery.Selection) {
+			license.Conditions = append(license.Conditions, strings.Trim(s.Text(), " \t\n"))
+		})
+		pageDoc.Find("ul.license-limitations > li").Each(func(i int, s *goquery.Selection) {
+			license.Limitations = append(license.Limitations, strings.Trim(s.Text(), " \t\n"))
+		})
+		license.Content = pageDoc.Find("#license-text").Text()
+
+		licenses = append(licenses, *license)
+	})
+
+	return licenses
+}
 
--- util.go
+++ util.go
@@ -3,39 +3,44 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
 func handleErr(errMsg string) {
-	fmt.Fprintf(os.Stderr, "%s %s\n", Bold(Color("error:", RED)), errMsg)
+	fmt.Fprintf(os.Stderr, "%s %s\n", Bold(Red("error:")), errMsg)
 	os.Exit(1)
 }
 
-var BLACK string = "0"
-var RED string = "1"
-var GREEN string = "2"
-var YELLOW string = "3"
-var BLUE string = "4"
-var PURPLE string = "5"
-var CYAN string = "6"
-var GRAY string = "7"
+func Black(text ...string) string {
+	return fmt.Sprintf("\x1b[30m%s\x1b[0m", strings.Join(text, " "))
+}
 
-func Color(text string, color string) string {
-	if color[0] == '#' {
-		r := color[1:3]
-		g := color[3:5]
-		b := color[5:7]
-		return fmt.Sprintf(
-			"\x1b[38;2;%d;%d;%dm%s\x1b[0m",
-			UnwrapOr(strconv.ParseUint(r, 16, 8))(0),
-			UnwrapOr(strconv.ParseUint(g, 16, 8))(0),
-			UnwrapOr(strconv.ParseUint(b, 16, 8))(0),
-			text,
-		)
-	} else {
-		return fmt.Sprintf("\x1b[3%sm%s\x1b[0m", color, text)
-	}
+func Red(text ...string) string {
+	return fmt.Sprintf("\x1b[31m%s\x1b[0m", strings.Join(text, " "))
+}
+
+func Green(text ...string) string {
+	return fmt.Sprintf("\x1b[32m%s\x1b[0m", strings.Join(text, " "))
+}
+
+func Yellow(text ...string) string {
+	return fmt.Sprintf("\x1b[33m%s\x1b[0m", strings.Join(text, " "))
+}
+
+func Blue(text ...string) string {
+	return fmt.Sprintf("\x1b[34m%s\x1b[0m", strings.Join(text, " "))
+}
+
+func Purple(text ...string) string {
+	return fmt.Sprintf("\x1b[35m%s\x1b[0m", strings.Join(text, " "))
+}
+
+func Cyan(text ...string) string {
+	return fmt.Sprintf("\x1b[36m%s\x1b[0m", strings.Join(text, " "))
+}
+
+func Gray(text ...string) string {
+	return fmt.Sprintf("\x1b[37m%s\x1b[0m", strings.Join(text, " "))
 }
 
 func Italic(text ...string) string {
